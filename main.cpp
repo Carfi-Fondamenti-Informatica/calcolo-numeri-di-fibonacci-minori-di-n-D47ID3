71e96c2c0dd1140cd8c5c5652fddecae2bbc0682
D47ID3 <91840256+D47ID3@users.noreply.github.com>
2021-11-14 22:30:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+   int n=0, i=i, b=i, a=0, c;
+   cin>>n; 
+   
+   cout<<b<<endl;
+   for (i=0,i<n,i++){
+      c=a+b;
+      cout<<c<<endl;
+      a=b;
+      b=c;
+   }
 }
